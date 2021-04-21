4ef5086c5ae6f13b4bf70932291a20032a006c60
kiel <kiel.wood@stratusgrid.com>
2021-04-21 15:57:36 -0400

    removed unnecessary import

--- bin/vue-cdk-demo.ts
+++ bin/vue-cdk-demo.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { VueCdkDemoDeploymentStage, VueCdkDemoPipelineStack } from '../lib/vue-cdk-demo-pipeline-stack';
+import { VueCdkDemoPipelineStack } from '../lib/vue-cdk-demo-pipeline-stack';
 
 const app = new cdk.App();
 new VueCdkDemoPipelineStack(app, 'VueCdkDemoPipelineStack');
